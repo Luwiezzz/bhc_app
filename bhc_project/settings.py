72cc40e9055a272379d5f6c08ed2868d8ea9d679
luwiezzz <luwiezsergio09@gmail.com>
2023-01-05 00:17:14 +0800

    Final Changes

--- bhc_project/settings.py
+++ bhc_project/settings.py
@@ -141,7 +141,7 @@ AUTH_USER_MODEL = 'bhc_app.registration'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bhcsalawag03@gmail.com'
-EMAIL_HOST_PASSWORD = 'jgoqbkwyayvbqrxc'
+EMAIL_HOST_USER = 'bhcsalawag@gmail.com'
+EMAIL_HOST_PASSWORD = 'rksrcgmltcbvdjkf'
 
 
